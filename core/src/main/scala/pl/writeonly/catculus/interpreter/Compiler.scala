37d28c2e20fdc8b4279009e7caea101d46555cd1
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-08-14 14:37:04 +0200

    Update Compiler.scala
    Signed-off-by: Kamil Adam <kamil.adam.zabinski@gmail.com>

--- core/src/main/scala/pl/writeonly/catculus/interpreter/Compiler.scala
+++ core/src/main/scala/pl/writeonly/catculus/interpreter/Compiler.scala
@@ -12,3 +12,31 @@ def compile(l: Lambda): Combinator = l match {
   case Abs(x, App(y, z)) => Apply(S, Apply(compile(Abs(x, y)), compile(Abs(x, z))))
   case _ => throw new Exception("Unsupported lambda term")
 }
+
+def compile(l: Lambda): Combinator = l match {
+  case Var(x) => I
+  case Abs(x, Var(y)) if x == y => I
+  case Abs(x, Var(y)) => K
+  case Abs(x, App(y, z)) if !freeVariables(z).contains(x) => Apply(B, compile(y))
+  case Abs(x, App(App(y, z1), z2)) if !freeVariables(z2).contains(x) => Apply(C, compile(y))
+  case Abs(_, App(f, x)) => Apply(Apply(S, compile(Abs(x, f))), compile(x))
+  case _ => throw new Exception("Unsupported lambda term")
+}
+
+def interpret(comb: Combinator): Combinator = comb match {
+  case I => I
+  case K => K
+  case S => S
+  case Apply(I, x) => x
+  case Apply(K, x) => K
+  case Apply(Apply(K, x), y) => x
+  case Apply(Apply(Apply(S, x), y), z) =>
+    val first = Apply(x, z)
+    val second = Apply(y, z)
+    Apply(first, second)
+  case Apply(f, x) =>
+    val reducedF = interpret(f)
+    val reducedX = interpret(x)
+    interpret(Apply(reducedF, reducedX))
+  case _ => throw new Exception("Unsupported combinator application")
+}
